9b1ad19d1106d765ab4f13e12a5768faa396946a
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-14 17:08:36 +0200

    GatewayController remove REST calls from service

--- src/main/java/com/monov/student/repository/StudentRepository.java
+++ src/main/java/com/monov/student/repository/StudentRepository.java
@@ -2,10 +2,11 @@ package com.monov.student.repository;
 
 import com.monov.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long>, JpaSpecificationExecutor<Student> {
 
     Student findByStudentId(Long studentId);
 
