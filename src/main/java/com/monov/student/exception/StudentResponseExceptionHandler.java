2005c49de86170b0f032565796c338a009ea10c1
RadoslavMonov <radoslav.monov@estafet.com>
2022-01-19 18:17:25 +0200

    ErrorHandlingAndResponses add changed property in response handler

--- src/main/java/com/monov/student/exception/StudentResponseExceptionHandler.java
+++ src/main/java/com/monov/student/exception/StudentResponseExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class StudentResponseExceptionHandler extends ResponseEntityExceptionHandler {
-    @Value("${exceptions.itemnotfound.message}")
+    @Value("${exceptions.item-not-found.message}")
     private String itemNotFoundMessage;
 
     @ExceptionHandler(ItemNotFoundException.class)
